7191d03e02a259c4d445d6112de37e4b7a240387
K-F.Hagman <fredde.hagman@gmail.com>
2021-12-14 01:16:54 +0100

    new layout

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ bs_btn.addEventListener("click", bs_layer)
 
 function bs_layer() {
     document.getElementById("button_container").classList.add("hide")
+    document.getElementById("zobi_quotes").classList.add("hide")
     document.getElementById("bs_layer").classList.remove("hide")
+    
+    document.getElementById("algozobi_logo").classList.remove("down_nm")
+    document.getElementById("algozobi_logo").classList.add("up_nm")
+    
 }
+
